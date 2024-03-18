25ea0b0b56c7520e12f736963410fcbe5e29b34f
khalil <h-adji_tech@proton.me>
2024-03-18 23:02:36 +0100

    added cli helper

--- main.go
+++ main.go
@@ -8,12 +8,28 @@ import (
 )
 
 func main() {
-	mathExpr := flag.String("expr", "", "Mathematical expression to evaluate. Example : 1/2 - (4 - cos(10+2))")
+	mathExpr := flag.String("expr", "", "Mathematical expression to evaluate. Example : 1/2 - (4 - cos(10^2))")
+	supportedFunctions := flag.Bool("S", false, "Shows the list of mathematical operations supported by this calculator")
 	showTokens := flag.Bool("include-tokens", false, "Add looging the parsed tokens to the output")
 	showAst := flag.Bool("include-ast", false, "Add looging the evaluated ast to the output")
 	radianToDegree := flag.Bool("use-degrees", false, "Enable Trigonometric functions evaluation using Degrees as a unit instead of Radians")
 	flag.Parse()
 	lexer := parser.NewLexer(*mathExpr)
+	if *supportedFunctions {
+		fmt.Println("The list of supported operations is the following:")
+		fmt.Println("\t=> cos()")
+		fmt.Println("\t=> acos()")
+		fmt.Println("\t=> sin()")
+		fmt.Println("\t=> asin()")
+		fmt.Println("\t=> tan()")
+		fmt.Println("\t=> atan()")
+		fmt.Println("\t=> sqrt()")
+		fmt.Println("\t=> * : for multiplication")
+		fmt.Println("\t=> ^ : for exponentiation")
+		fmt.Println("\t=> + : for addition")
+		fmt.Println("\t=> - : for substraction")
+		return
+	}
 	if *showTokens {
 		fmt.Printf("Tokens : %v \n\n", lexer.Tokens())
 	}
@@ -26,5 +42,9 @@ func main() {
 
 	}
 	value := parser.TreeWalk(&ast.Root, *radianToDegree)
-	fmt.Println(value)
+	if *mathExpr == "" {
+		fmt.Println("No expression provided, Please use --help")
+		return
+	}
+	fmt.Printf("%s = %.2f\n", *mathExpr, value)
 }
